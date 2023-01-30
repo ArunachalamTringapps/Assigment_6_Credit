2a309ad11a7fd0a1e9ff4b2a685d2c94e5720530
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-01-30 14:50:27 +0530

    inital commend

--- src/main/java/org/example/Credit.java
+++ src/main/java/org/example/Credit.java
@@ -3,15 +3,17 @@ import java.util.*;
 import java.util.logging.*;
 class Card implements Cloneable{
     String name;
-    String  no1;
+    int  no1;
     String date;
-    Card(String name,String no1,String date){
+    Card(String name,int no1,String date){
      this.name=name;
      this.date=date;
      this.no1=no1;
     }
-    public boolean equalling(String no2){
-        return no1.equals(no2);
+    public boolean equalling(int no2){
+        String no4=String.valueOf(no2);
+        String no3=String.valueOf(no1);
+        return no3.equals(no4);
     }
 
     @Override
@@ -27,12 +29,13 @@ public class Credit {
         l.info("Enter the cardholders name: ");
         String name=sc.nextLine();
         l.info("Enter the card number:");
-        String no1=sc.nextLine();
+        int no1=sc.nextInt();
+        sc.nextLine();
         l.info("Enter the card Expiration date:");
         String date=sc.nextLine();
         Card c1=new Card(name,no1,date);
         l.info("Enter the card number to check given card number is available:");
-        String no2=sc.nextLine();
+        int no2=sc.nextInt();
         Card c2=(Card) c1.clone();
         l.log(Level.INFO, () -> "The result is: " +c2.equalling(no2));
     }
