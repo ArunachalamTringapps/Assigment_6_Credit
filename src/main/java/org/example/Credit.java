dadd49702ada9b3b5604ff3f361467cce17c5e49
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-01-30 17:05:12 +0530

    inital commend

--- src/main/java/org/example/Credit.java
+++ src/main/java/org/example/Credit.java
@@ -22,21 +22,26 @@ class Card implements Cloneable{
     }
 }
 public class Credit {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         Scanner sc=new Scanner(System.in);
         Logger l=Logger.getLogger("com.api.jar");
         l.info("welcome guys");
-        l.info("Enter the cardholders name: ");
-        String name=sc.nextLine();
-        l.info("Enter the card number:");
-        int no1=sc.nextInt();
-        sc.nextLine();
-        l.info("Enter the card Expiration date:");
-        String date=sc.nextLine();
-        Card c1=new Card(name,no1,date);
-        l.info("Enter the card number to check given card number is available:");
-        int no2=sc.nextInt();
-        Card c2=(Card) c1.clone();
-        l.log(Level.INFO, () -> "The result is: " +c2.equalling(no2));
+        try {
+            l.info("Enter the cardholders name: ");
+            String name = sc.nextLine();
+            l.info("Enter the card number:");
+            int no1 = sc.nextInt();
+            sc.nextLine();
+            l.info("Enter the card Expiration date:");
+            String date = sc.nextLine();
+            Card c1 = new Card(name, no1, date);
+            l.info("Enter the card number to check given card number is available:");
+            int no2 = sc.nextInt();
+            Card c2 = (Card) c1.clone();
+            l.log(Level.INFO, () -> "The result is: " + c2.equalling(no2));
+        }catch (Exception ex){
+            l.log(Level.INFO, () -> "Error occur: " + ex);
+
+        }
     }
 }
